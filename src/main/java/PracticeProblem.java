60558bf3ae95177cda6116307637ee98d74ec6ce
JackyLui1215 <343344461@tdsb.ca>
2026-02-11 16:21:14 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -10,22 +10,42 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		int two = 2;
+		int three = 3;
+		int five = 5;
+		System.out.println(two + three + five);
+		System.out.println(two * three * five);
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int ten = 10;
+		int three = 3;
+		System.out.println(ten / three);
+		System.out.println(ten % three);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		double numberOne= 5.3;
+		double numberTwo= 5.2;
+		System.out.println(numberOne / numberTwo);
+
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		int two = 2;
+		int three = 3;
+		int four = 4;
+		System.out.println((two + three) * four);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		double five = 5;
+		five = five + 3.5;
+		System.out.println(five);
 	}
 
 }
